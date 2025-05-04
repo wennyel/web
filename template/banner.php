fdef642c0c668ae3267a2089b50da0b0d6d35be5
wennyel <eniel1996@gmail.com>
2025-05-04 20:36:32 -0300

    Add files via upload

---
+++ template/banner.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- CabaliCore 2018 , www.cabalicore.com.br -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+	<meta charset="utf-8" />
+	
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+    <link href="../css/css-reset.css" rel="stylesheet">
+    <link href="../css/slide.css" rel="stylesheet">
+    <script src="../js/jquery-2.1.4.min.js"></script>
+  	<script src="../js/modalx.js"></script>
+  	<script src="../js/slider.js"></script>
+</head>
+<div class="slider">
+							<div class="next"> </div>
+							<div class="prev"> </div>
+							<div class="slides">
+								<div class="slide active">
+									<img src="../images/concurso.jpg" alt="">
+									<h2></h2>
+									<p></p>
+									<div><a href="#">Noticia</a> <span>29-05-2018</span>  -  convidamos a todos para uma nova aventura, aqui tem diversão que nunca acaba.</div>
+								</div>
+									<div class="slide">
+									<img src="../images/CARNAGE_SLIDER.png"  alt="">
+									<!--<div><a href="#">N</a> <span>14-06-2018</span>  - o melhor MMORPG private já visto no brasil.</div>-->
+								</div>
+								<div class="slide">
+									<img src="../images/SLIDER_DOACAO.png"  alt="">
+								</div>
+								
+								<div class="slide">
+									<!--<h2>UPDATE - Honra e Glória</h2>
+									<p>Novos sets chegam por aqui, confira!</p>-->
+									<img src="../images/SLIDER_YOUTUBE.png"  alt="">
+									<!--<div><a href="#">Update</a> <span>20.02.18</span>  -  novidades exclusivas.</div>-->
+								</div>
+								
+							</div>
+							<div class="navigation">
+							</div>
+						</div><!-- slider-block -->
